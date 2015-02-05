26674f4408ea286d00be043f8188c8d8d7ee2337
Wan Lizhou <lizhouwan@hotmail.com>
2015-02-05 13:20:52 +0800

    Some tiny change in testcase

--- fnat_testset/testcase/testcategory_1/testgroup_1.py
+++ fnat_testset/testcase/testcategory_1/testgroup_1.py
@@ -13,11 +13,8 @@ class testclass_1:
 
     def testmethod_1(self):
         print "Method testmethod_1 in class testclass_1"
-        print "PATH = " +  os.environ['PYTHONPATH']
         d = FnatDevice("0c05b1d4dbc84ce8")
         d.press.home()
-        time.sleep(0.5)
-        d.screenshot("home.png")
         assert True
 
     def testmethod_2(self):
