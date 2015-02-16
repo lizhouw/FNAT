c1c0d007a3ae7ff813fa5f8a811d08d60eea57fc
Wan Lizhou <lizhouwan@hotmail.com>
2015-02-16 17:05:27 +0800

    Add data_driver.py

---
+++ fnat_runner/data_driver.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python 
+
+class data_driver:
+    def __init__(self, data_file):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def getParamter(self, param_name):
+        pass
